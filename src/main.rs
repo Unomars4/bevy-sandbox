ad70d629e081400074f060004208a98fc9c634da
Une Nyide <une.nyide@gmail.com>
2025-04-03 22:53:23 +0200

    feat: add camera component to world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{math::f32, prelude::*, sprite::Anchor, text::cosmic_text::ttf_parser::gpos::Anchor};
+use bevy::{prelude::*, sprite::Anchor};
 
 const GROUND_LEVEL: f32 = -100.0;
 const PLAYER_X: f32 = -300.0;
@@ -10,13 +10,15 @@ struct Player;
 struct Velocity(Vec3);
 
 fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+
     commands.spawn((
         Player,
         Sprite {
             color: Color::srgb(0.5, 1.0, 0.5),
             custom_size: Some(Vec2::new(30.0, 50.0)),
             anchor: Anchor::BottomCenter,
-            ..Default::default()
+            ..default()
         },
         Transform::from_xyz(PLAYER_X, GROUND_LEVEL, 0.0),
         Velocity(Vec3::ZERO),
@@ -27,7 +29,7 @@ fn setup(mut commands: Commands) {
             color: Color::srgb(0.5, 0.5, 0.5),
             custom_size: Some(Vec2::new(800.0, 10.0)),
             anchor: Anchor::TopLeft,
-            ..Default::default()
+            ..default()
         },
         Transform::from_xyz(-400.0, GROUND_LEVEL, 0.0),
     ));

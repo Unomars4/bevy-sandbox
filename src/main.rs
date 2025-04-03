1baffe9608a6be7df0357716d9d660d90fd61a6f
Une Nyide <une.nyide@gmail.com>
2025-04-03 22:35:17 +0200

    feat: add constants, player and velocity components

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use bevy::prelude::*;
+use bevy::{math::f32, prelude::*};
+
+const GROUND_LEVEL: f32 = -100.0;
+const PLAYER_X: f32 = -300.0;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Velocity(f32);
 
 fn main() {
     println!("Commence Endgame 🚀");

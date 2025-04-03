ef89f32bd117e694870162ee47ead955fc93d47e
Une Nyide <une.nyide@gmail.com>
2025-04-03 22:41:22 +0200

    feat: add player entity to world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{math::f32, prelude::*};
+use bevy::{math::f32, prelude::*, sprite::Anchor, text::cosmic_text::ttf_parser::gpos::Anchor};
 
 const GROUND_LEVEL: f32 = -100.0;
 const PLAYER_X: f32 = -300.0;
@@ -8,6 +8,18 @@ struct Player;
 
 #[derive(Component)]
 struct Velocity(f32);
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Player,
+        Sprite {
+            color: Color::Srgba(0.5, 1.0, 0.5),
+            custom_size: Some(Vec2::new(30.0, 50.0)),
+            anchor: Anchor::BottomCenter,
+            ..Default::default()
+        },
+    ));
+}
 
 fn main() {
     println!("Commence Endgame 🚀");
